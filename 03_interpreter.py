3b2f446488c010458cc25db093610029a36597c3
whitcr <whitcr0k@gmail.com>
2024-03-31 18:49:38 +0200

    fixed

--- 03_interpreter.py
+++ 03_interpreter.py
@@ -131,10 +131,12 @@ class Parser:
     def term(self):
         """ Парсер для 'term' правил граматики."""
         node = self.factor()
-        while self.current_token.type in (TokenType.MUL,):
+        while self.current_token.type in (TokenType.MUL, TokenType.DIV):
             token = self.current_token
             if token.type == TokenType.MUL:
                 self.eat(TokenType.MUL)
+            elif token.type == TokenType.DIV:
+                self.eat(TokenType.DIV)
 
             node = BinOp(left=node, op=token, right=self.factor())
 
@@ -150,20 +152,18 @@ class Parser:
             self.eat(TokenType.LPAREN)
             node = self.expr()
             self.eat(TokenType.RPAREN)
-            return node(2 + 3) * 4
+            return node 
 
     def expr(self):
         """ Парсер для арифметичних виразів. """
         node = self.term()
 
-        while self.current_token.type in (TokenType.PLUS, TokenType.MINUS, TokenType.DIV):
+        while self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
             token = self.current_token
             if token.type == TokenType.PLUS:
                 self.eat(TokenType.PLUS)
             elif token.type == TokenType.MINUS:
                 self.eat(TokenType.MINUS)
-            elif token.type == TokenType.DIV:
-                self.eat(TokenType.DIV)
 
             node = BinOp(left=node, op=token, right=self.term())
 
